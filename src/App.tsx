d11e8fcc853149bcb72c93b7a70b12aca0cc4ce0
Mohammad Kaif <mdkaifprofession@gmail.com>
2024-09-03 12:22:08 +0545

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Widget1 from "./components/Widget1";
+
+function App() {
+  return (
+    <div className="container bg-back h-screen grid grid-cols-2 text-white">
+      <div></div>
+      <div>
+        <Widget1 />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Widget1.tsx
@@ -0,0 +1,8 @@
+type Props = {};
+
+const Widget1 = (props: Props) => {
+  return <div className="bg-widget">
+  </div>;
+};
+
+export default Widget1;
